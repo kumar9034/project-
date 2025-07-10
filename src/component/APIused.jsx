60652b71cc6779b2d3f82e7f0687ae6655ef2461
kumar9034 <kumarrajput040202003@gmail.com>
2025-07-10 16:11:38 +0530

    feat: implement APIused component for audio track playback and integrate into Section5

---
+++ src/component/APIused.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useRef, useState } from 'react'
+import Section5 from './section5';
+import { FaPlay } from "react-icons/fa";
+import { FaPause } from "react-icons/fa6";
+
+const APIused = () => {
+
+    const [track, settrack] = useState([])
+    const audioRef = useRef(null);
+    const [playingId, setPlayingId] = useState(null);
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+            const url = 'https://spotify81.p.rapidapi.com/playlist?id=37i9dQZF1DX4Wsb4d7NKfP';
+            const options = {
+                method: 'GET',
+                headers: {
+                    'x-rapidapi-key': '4491fe8b02msh6de10e0b55a0a80p1dccfajsn2e46febdfb99',
+                    'x-rapidapi-host': 'spotify81.p.rapidapi.com'
+                }
+            };
+
+            try {
+                const response = await fetch(url, options);
+                const result = await response.json();
+                const items = result.tracks.items
+                const track = items.map((item) => (item.track))
+                console.log(track);
+                settrack(track)
+
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchData(); // Call the async function
+    }, []);
+
+    const togglePlay = (track) => {
+    if (!track.preview_url) return;
+
+    if (playingId === track.id) {
+      audioRef.current.pause();
+      setPlayingId(null);
+    } else {
+      audioRef.current.src = track.preview_url;
+      audioRef.current.play();
+      setPlayingId(track.id);
+      audioRef.current.onended = () => setPlayingId(null);
+    }
+  };
+
+    return (
+        <>
+        <div className=''>
+            {track.map((items) => (
+                <div className='w-full h-10 hover:bg-[#131313] cursor-pointer  flex justify-between px-8  '
+                onClick={() => togglePlay(items)}>
+                    <div className='flex gap-3'>
+                        <div className='flex mt-2 mr-2 '>
+                            <button  >{playingId === items.id ? <FaPause color='#424242' size={18}  /> : <FaPlay color='#424242' size={18} />}</button>
+                        </div>
+                        <div className='leading-none mt-2'>
+                            <h1 className='text-white font-[600] text-[14px] '>{items.name}</h1>
+                            <h1 className='text-[#424242] font-[600] text-[10px] mt-1 '>{items.artists[0].name}</h1>
+                        </div>
+                    </div>
+                    <span className='text-[13px] font-[600] text-white mt-2 '>{
+                        Math.floor(items.duration_ms / 60000)
+                    }:
+                    {
+                            String(Math.floor((items.duration_ms % 60000) / 1000)).padStart(2, '0')
+                        }</span>
+                        <div className='hidden'> 
+                            <audio ref={audioRef} ></audio>
+                        </div>
+                </div>
+
+            ))}
+
+        </div>
+
+
+        </>
+    )
+}
+
+
+export default APIused
--- src/component/Section5.jsx
+++ src/component/Section5.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import { IoMdAddCircleOutline } from "react-icons/io";
+import { FaPlayCircle } from "react-icons/fa";
+import { IoPlaySkipBackSharp } from "react-icons/io5";
+import { IoPlaySkipForward } from "react-icons/io5";
+import { BsThreeDots } from "react-icons/bs";
+import APIused from './APIused';
 
 const Section5 = () => {
   return (
@@ -12,16 +18,39 @@ const Section5 = () => {
       </div>
       <div className='flex'>
         <img className='w-full h-[100vh]  object-cover' src="image-32.svg" alt="" />
-        <div className='w-[75vw] h-[80vh] bg-black rounded-lg border-[#424242] border-1 ml-40 mt-15 absolute'>
-            <div className='w-full h-40 bg-[#131313] pt-5 flex justify-between '>
+        <div className='w-[75vw] h-[80vh] bg-black  rounded-lg border-[#424242] border-1 ml-40 mt-15 absolute'>
+            <div className='w-full h-40 bg-[#131313] pt-5   flex justify-between '>
                 <div className='w-[60vw] flex'>
                 <img className='w-30 h-30 ml-10 object-cover' src="image-33.jpg" alt="" />
-                <h1 className='bebas-font text-4xl text-white ml-4'>The Sounds Project 8 - DAY 1</h1>
+                <div className='mt-5'>
+                <h1 className='font-[700] text-2xl text-white ml-4'>The Sounds Project 8 - DAY 1</h1>
+                <div className='flex gap-4  mt-1 items-center'>
+                <div className='w-15 h-5 bg-[#bcbcbc] bebas-font text-sm flex justify-center items-center ml-4 rounded-md'>preview </div>
+                <span className='text-lg font-[600] text-[#424242]'>the sound project</span>
+                </div>
+                <div className='flex ml-4 mt-1 gap-3'>
+                    <IoMdAddCircleOutline color='#bcbcbc' size={24}/>
+                    <p className='text-sm text-[#bcbcbc] font-[500]'>Save on spotify</p>
+                </div>
+
+                </div>
                 </div>
                 <div className='w-[40vw] '>
                     <img className='ml-80' src="image-34.svg" alt="" />
+                    <div className='flex  gap-5 mt-12 ml-40'>
+                    <div className='flex mt-2 gap-8'>
+                      <IoPlaySkipBackSharp color='#424242' size={25}/>
+                      <IoPlaySkipForward color='#424242'size={25} />
+                    </div>
+                    <BsThreeDots color='#bcbcbc' size={25} className='mt-2' />
+                    <FaPlayCircle color='white' className='mt-' size={40} />
+
+                    </div>
                 </div>
             </div>
+                <div className='w-full h-76 overflow-y-auto whitespace-nowrap scrollbar-hide'>
+                <APIused/>
+                </div>
         </div>
       </div>
     </div>
