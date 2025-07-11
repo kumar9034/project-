05e26dd1e57fdbe887ae3718d71a3497a02614a9
kumar9034 <kumarrajput040202003@gmail.com>
2025-07-11 23:52:48 +0530

    fix: update image hover effect in Card2 component for smoother transition

--- src/component/Card2.jsx
+++ src/component/Card2.jsx
@@ -4,7 +4,7 @@ const Card2 = React.forwardRef(({ name, photoes }, ref) => {
   return (
        <div>
          <div ref={ref} className='w-50 h-[26rem] rounded-lg '>
-           <img className='rounded-lg group-hover:translate-y-0 transition-all duration-500 ease-in-out' src={photoes} alt="" />
+           <img className='rounded-lg hover:scale-105' src={photoes} alt="" />
            <p className='bebas-font text-white text-md mt-2 w-9 h-10 flex-wrap flex '>{name}</p>
            <p className='flex text-white bebas-font gap-2 '> <span><img className='w-5 h-5  ' src="image-38.svg " alt="" /></span> youtube</p>
          </div>
