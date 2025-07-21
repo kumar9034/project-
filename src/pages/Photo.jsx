fd186c1dbb365dc685ffe37caab980be6344f67a
kumar9034 <kumarrajput040202003@gmail.com>
2025-07-21 15:13:58 +0530

    fix: adjust margin in Photo component for improved layout consistency

--- src/pages/Photo.jsx
+++ src/pages/Photo.jsx
@@ -276,7 +276,7 @@ const Photo = () => {
 
                                 <img className='w-full sm:h-[30rem] h-[40vh]  object-cover' src={selecte.image} alt="" />
                                 <p className='text-md font-[400] h-25 mt-3'>{selecte.description}</p>
-                                <div className='flex mt-[5vh] '>
+                                <div className='flex mt-[4vh] '>
                                 <span><img className='w-7 h-7 ' src="image-52.webp" alt="" /></span>
                                 <p className='bebas-font text-lg  ml-3 '>@{selecte.name}</p>
 
